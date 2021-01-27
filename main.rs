6c9709e3bb25e49ebd39b08bfbad664ee7399613
Tristan Charpentier <tristan_charpentier@hotmail.com>
2021-01-27 20:53:39 +0100

    Cleaned up

--- main.rs
+++ main.rs
@@ -3,29 +3,26 @@ use std::io::Read;
 use std::io::{Error,ErrorKind};
 use std::io;
 use std::string::String;
-//use std::result::Result;
 
 fn read_levelpack(filename: &str, level_num: usize) -> io::Result<String> {
+    // Read in entire file to mut string
     let mut file = File::open(&filename)?;
     let mut text = String::new();
     file.read_to_string(&mut text)?;
-    //let level: Vec = vec!(text.split('\n').collect()?);
+
+    // Find nth level(line), if not found, throw error
     match text.split('\n').nth(level_num) {
         None => Err(Error::new(ErrorKind::Other, "Could not find specified level")),
         Some(l) => Ok(l.to_string()),
     }
-    /*
-    match level.get(level_num) {
-        Some(l) => (l.to_string()),
-        _ => Err(Error::new(ErrorKind::Other, "Could not find specified level")),
-    }*/
-    //Ok(level.to_string())
 }
 
 fn main() {
-    // Read in file
+    // Init
     let file: &str = "levels/levelpack_0.txt";
-    let level_num: usize = 4;
-    let res = read_levelpack(file, level_num).expect("Err: Couldn't find level");
+    let level_num: usize = 100;
+    // Read in level from levelpack
+    let mut res = read_levelpack(file, level_num).expect("Err: Couldn't find level/levelpack");
+    
     println!("{:?}", res);
 }
